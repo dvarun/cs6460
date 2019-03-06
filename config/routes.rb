c4a48521e81b4b7ccbf20c72bbcfe9d582bfca6f
Varun D <dvarun@hotmail.com>
2019-03-06 23:06:38 +0700

    added course page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'groups/new'
+  get 'groups/create'
+  resources :courses
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_04_143626) do
+ActiveRecord::Schema.define(version: 2019_03_06_160315) do
+
+  create_table "courses", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "description"
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.boolean "instructor_allowed", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
