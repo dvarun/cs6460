e7b8cf8e9d9818ec202f50e72e531858fbf11355
Varun D <dvarun@hotmail.com>
2019-03-08 00:06:11 +0700

    refine UI and route root

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :groups
   resources :courses
   devise_for :users
+
+  root "courses#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
