1c95e7e0968f5a2d60f46cf7e7e37f1aa7ca6a87
Varun D <dvarun@hotmail.com>
2019-03-18 00:20:07 +0700

    remove one migration file

--- db/migrate/20190307172819_add_columnin_gmember.rb
+++
@@ -1,6 +0,0 @@
-class AddColumninGmember < ActiveRecord::Migration[5.2]
-  def change
-    add_column :group_members, :user_id, :integer
-    add_column :group_members, :group_id, :integer
-  end
-end
