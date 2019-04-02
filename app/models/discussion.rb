11473cf8cbf51da1fa3850cc07ec62067fadd0da
Varun D <dvarun@hotmail.com>
2019-04-02 21:57:49 +0700

    added sentiment grabber in discussion

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,4 +1,8 @@
 class Discussion < ApplicationRecord
   belongs_to :group
   belongs_to :user
+
+  after_create do |discussion|
+    ApiCaller.analyze("Discussion", discussion.id, discussion.comment, discussion.user_id)
+  end
 end
